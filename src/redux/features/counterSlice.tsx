60550cbda9d086355da9fb5401f0d132732d633f
NaimurAlltime <naimur2231@gmail.com>
2024-01-18 15:56:22 +0600

    redux toolkit configuration done

---
+++ src/redux/features/counterSlice.tsx
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface CounterState {
+  count: number;
+}
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+    incrementByValue: (state, action: PayloadAction<number>) => {
+      state.count = state.count + action.payload;
+    },
+    decrementByValue: (state, action: PayloadAction<number>) => {
+      state.count = state.count - action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByValue, decrementByValue } =
+  counterSlice.actions;
+
+export default counterSlice.reducer;
